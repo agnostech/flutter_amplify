77516c83d6cdb9f647fbe2b2aa0679ee556048f1
Vishal Dubey <yzvishal.vd@gmail.com>
2020-03-13 21:01:28 +0530

    added pubsub methods for Android

--- flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutterawsamplifypubsub/FlutterAwsAmplifyPubSub.kt
+++ flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutterawsamplifypubsub/FlutterAwsAmplifyPubSub.kt
@@ -1,8 +1,13 @@
 package `in`.agnostech.flutterawsamplifypubsub
 
-import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager
+import android.icu.util.DateInterval
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobileconnectors.iot.*
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
+import com.amazonaws.services.iot.AWSIotClient
+import com.amazonaws.services.iot.model.AttachPolicyRequest
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import kotlin.math.min
 
@@ -82,6 +87,172 @@ class FlutterAwsAmplifyPubSub {
             }
         }
 
+        fun getOfflinePublishQueueBound(result: MethodChannel.Result) = result.success(awsIotManager.offlinePublishQueueBound)
+
+        fun setOfflinePublishQueueBound(result: MethodChannel.Result, bound: Int) {
+            try {
+                awsIotManager.offlinePublishQueueBound = bound
+            } catch (e: Error) {
+                result.error("Error", "Error setting offline publish queue bond", e.localizedMessage)
+            }
+        }
+
+        fun getDrainingInterval(result: MethodChannel.Result) = result.success(awsIotManager.drainingInterval)
+
+        fun setDrainingInterval(result: MethodChannel.Result, interval: Long) {
+            try {
+                awsIotManager.drainingInterval = interval
+            } catch (e: Error) {
+                result.error("Error", "Error setting draining interval", e.localizedMessage)
+            }
+        }
+
+        fun fullPublishQueueKeepsOldestMessages(result: MethodChannel.Result) = result.success(awsIotManager.fullPublishQueueKeepsOldestMessages())
+
+        fun setFullQueueToKeepOldestMessages(result: MethodChannel.Result) {
+            try {
+                awsIotManager.setFullQueueToKeepOldestMessages()
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error setting queue for old messages", e.localizedMessage)
+            }
+        }
+
+        fun setFullQueueToKeepNewestMessages(result: MethodChannel.Result) {
+            try {
+                awsIotManager.setFullQueueToKeepNewestMessages()
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun getKeepAlive(result: MethodChannel.Result) = result.success(awsIotManager.keepAlive)
+
+        fun setKeepAlive(result: MethodChannel.Result, keepAlive: Int) {
+            try {
+                awsIotManager.keepAlive = keepAlive
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun getMqttLastWillAndTestament(result: MethodChannel.Result) {
+            val lastWill = awsIotManager.mqttLastWillAndTestament
+            result.success(hashMapOf(
+                    "message" to lastWill.message,
+                    "topic" to lastWill.topic,
+                    "qos" to lastWill.qos
+            ))
+        }
+
+        fun setMqttLastWillAndTestament(result: MethodChannel.Result, qos: Int, message: String, topic: String) {
+            try {
+                awsIotManager.mqttLastWillAndTestament = AWSIotMqttLastWillAndTestament(
+                        topic, message, if (qos == 0) AWSIotMqttQos.QOS0 else AWSIotMqttQos.QOS1
+                )
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun setAutoResubscribe(result: MethodChannel.Result, enabled: Boolean) {
+            try {
+                awsIotManager.setAutoResubscribe(enabled)
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun setCleanSession(result: MethodChannel.Result, cleanSession: Boolean) {
+            try {
+                awsIotManager.setCleanSession(cleanSession)
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun connect(result: MethodChannel.Result, connectEvent: EventChannel.EventSink) {
+            try {
+                awsIotManager.connect(AWSMobileClient.getInstance()) { status, throwable ->
+                    if (throwable != null) {
+                        connectEvent.error("Error", "Error connecting", throwable.localizedMessage)
+                    }
+                    connectEvent.success(status.name)
+                }
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun disconnect(result: MethodChannel.Result) {
+            try {
+                awsIotManager.disconnect()
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error disconnecting", e.localizedMessage)
+            }
+        }
+
+        fun subscribeToTopic(result: MethodChannel.Result, messageEvent: EventChannel.EventSink, subscriptionTopic: String, qos: Int) {
+            try {
+                awsIotManager.subscribeToTopic(subscriptionTopic, if (qos == 0) AWSIotMqttQos.QOS0 else AWSIotMqttQos.QOS1, object : AWSIotMqttSubscriptionStatusCallback {
+                    override fun onSuccess() {
+                        result.success(true)
+                    }
+
+                    override fun onFailure(exception: Throwable) {
+                        result.error("Error", "Error subscribing to $subscriptionTopic", exception.localizedMessage)
+                    }
+
+                }) { topic, data ->
+                    messageEvent.success(hashMapOf(
+                            "topic" to topic,
+                            "data" to String(data, Charsets.UTF_8)
+                    ))
+                }
+                result.success(true)
+            } catch (e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun unsubscribeTopic(result: MethodChannel.Result, topic: String) {
+            try {
+                awsIotManager.unsubscribeTopic(topic)
+                result.success(true)
+            } catch(e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
+
+        fun attachPolicy(result: MethodChannel.Result, policyName: String, awsRegion: String) {
+            try {
+                val attachPolicyReq = AttachPolicyRequest()
+                attachPolicyReq.policyName = policyName
+                attachPolicyReq.target = AWSMobileClient.getInstance().identityId
+                val iotAndroidClient = AWSIotClient(AWSMobileClient.getInstance())
+                iotAndroidClient.setRegion(Region.getRegion(awsRegion))
+                iotAndroidClient.attachPolicy(attachPolicyReq)
+                result.success(true)
+            } catch(e: Error) {
+                result.error("Error", "Error attaching policy", e.localizedMessage)
+            }
+        }
+
+        fun publishString(result: MethodChannel.Result, payload: String, topic: String, qos: Int) {
+            try {
+                awsIotManager.publishString(payload, topic, if (qos == 0) AWSIotMqttQos.QOS0 else AWSIotMqttQos.QOS1)
+                result.success(true)
+            } catch(e: Error) {
+                result.error("Error", "Error", e.localizedMessage)
+            }
+        }
 
     }
 }
--- flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutterawsamplifypubsub/FlutterAwsAmplifyPubSubPlugin.kt
+++ flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutterawsamplifypubsub/FlutterAwsAmplifyPubSubPlugin.kt
@@ -6,17 +6,21 @@ import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class getConnectionStabilityTimeFlutterAwsAmplifyPubSubPlugin : FlutterPlugin {
+class FlutterAwsAmplifyPubSubPlugin : FlutterPlugin {
 
     private lateinit var methodChannel: MethodChannel
-    private lateinit var eventChannel: EventChannel
+    private lateinit var subsChannel: EventChannel
+    private lateinit var connectionChannel: EventChannel
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        methodChannel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_aws_amplify_pubsub/config")
+        methodChannel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "flutter_aws_amplify_pubsub/config")
         methodChannel.setMethodCallHandler(PubSubHandler())
 
-        eventChannel = EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_aws_amplify_pubsub/sub")
-        eventChannel.setStreamHandler(PubSubHandler())
+        subsChannel = EventChannel(flutterPluginBinding.flutterEngine.dartExecutor, "flutter_aws_amplify_pubsub/sub")
+        subsChannel.setStreamHandler(PubSubHandler())
+
+        connectionChannel = EventChannel(flutterPluginBinding.flutterEngine.dartExecutor, "flutter_aws_amplify_pubsub/pubsub_conn")
+        connectionChannel.setStreamHandler(PubSubHandler())
     }
 
     companion object {
@@ -24,13 +28,16 @@ class getConnectionStabilityTimeFlutterAwsAmplifyPubSubPlugin : FlutterPlugin {
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "flutter_aws_amplify_pubsub")
             channel.setMethodCallHandler(PubSubHandler())
-            val eventChannel = EventChannel(registrar.messenger(), "flutter_aws_amplify_pubsub/sub")
-            eventChannel.setStreamHandler(PubSubHandler())
+            val subsChannel = EventChannel(registrar.messenger(), "flutter_aws_amplify_pubsub/sub")
+            subsChannel.setStreamHandler(PubSubHandler())
+            val connectionChannel = EventChannel(registrar.messenger(), "flutter_aws_amplify_pubsub/pubsub_conn")
+            connectionChannel.setStreamHandler(PubSubHandler())
         }
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         methodChannel.setMethodCallHandler(null)
-        eventChannel.setStreamHandler(null)
+        subsChannel.setStreamHandler(null)
+        connectionChannel.setStreamHandler(null)
     }
 }
