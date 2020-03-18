eb479493ac3a53e71a833589b5408e85f7386f7d
Vishal Dubey <yzvishal.vd@gmail.com>
2020-03-18 12:13:06 +0530

    connected method calls from flutter to native code on Android

--- flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutter_aws_amplify_pubsub/PubSubHandler.kt
+++ flutter_aws_amplify_pubsub/android/src/main/kotlin/in/agnostech/flutter_aws_amplify_pubsub/PubSubHandler.kt
@@ -11,7 +11,39 @@ object PubSubHandler: MethodChannel.MethodCallHandler, EventChannel.StreamHandle
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
-            "initialize" -> ""
+            "initialize" -> FlutterAwsAmplifyPubSub.initialize(result, call.argument<String>("mqttClientId")!!, call.argument<String>("awsIoTEndPointUrl")!!)
+            "setMetricsIsEnabled" -> FlutterAwsAmplifyPubSub.setMetricsIsEnabled(result, call.argument<Boolean>("enabled")!!)
+            "isMetricsEnabled" -> FlutterAwsAmplifyPubSub.isMetricsEnabled(result)
+            "isAutoReconnect" -> FlutterAwsAmplifyPubSub.isAutoReconnect(result)
+            "setAutoReconnect" -> FlutterAwsAmplifyPubSub.setAutoReconnect(result, call.argument<Boolean>("autoReconnect")!!)
+            "setReconnectRetryLimits" -> FlutterAwsAmplifyPubSub.setReconnectRetryLimits(result, call.argument<Int>("minTimeout")!!, call.argument<Int>("maxTimeout")!!)
+            "getMinReconnectRetryTime" -> FlutterAwsAmplifyPubSub.getMinReconnectRetryTime(result)
+            "getMaxReconnectRetryTime" -> FlutterAwsAmplifyPubSub.getMaxReconnectRetryTime(result)
+            "getMaxAutoReconnectAttempts" -> FlutterAwsAmplifyPubSub.getMaxAutoReconnectAttempts(result)
+            "setMaxAutoReconnectAttempts" -> FlutterAwsAmplifyPubSub.setMaxAutoReconnectAttempts(result, call.argument<Int>("attempts")!!)
+            "setConnectionStabilityTime" -> FlutterAwsAmplifyPubSub.setConnectionStabilityTime(result, call.argument<Int>("time")!!)
+            "getConnectionStabilityTime" -> FlutterAwsAmplifyPubSub.getConnectionStabilityTime(result)
+            "isOfflinePublishQueueEnabled" -> FlutterAwsAmplifyPubSub.isOfflinePublishQueueEnabled(result)
+            "setOfflinePublishQueueEnabled" -> FlutterAwsAmplifyPubSub.setOfflinePublishQueueEnabled(result, call.argument<Boolean>("queueEnabled")!!)
+            "getOfflinePublishQueueBound" -> FlutterAwsAmplifyPubSub.getOfflinePublishQueueBound(result)
+            "setOfflinePublishQueueBound" -> FlutterAwsAmplifyPubSub.setOfflinePublishQueueBound(result, call.argument<Int>("bound")!!)
+            "getDrainingInterval" -> FlutterAwsAmplifyPubSub.getDrainingInterval(result)
+            "setDrainingInterval" -> FlutterAwsAmplifyPubSub.setDrainingInterval(result, call.argument<Long>("interval")!!)
+            "fullPublishQueueKeepsOldestMessages" -> FlutterAwsAmplifyPubSub.fullPublishQueueKeepsOldestMessages(result)
+            "setFullQueueToKeepOldestMessages" -> FlutterAwsAmplifyPubSub.setFullQueueToKeepOldestMessages(result)
+            "setFullQueueToKeepNewestMessages" -> FlutterAwsAmplifyPubSub.setFullQueueToKeepNewestMessages(result)
+            "getKeepAlive" -> FlutterAwsAmplifyPubSub.getKeepAlive(result)
+            "setKeepAlive" -> FlutterAwsAmplifyPubSub.setKeepAlive(result, call.argument<Int>("keepAlive")!!)
+            "getMqttLastWillAndTestament" -> FlutterAwsAmplifyPubSub.getMqttLastWillAndTestament(result)
+            "setMqttLastWillAndTestament" -> FlutterAwsAmplifyPubSub.setMqttLastWillAndTestament(result, call.argument<Int>("qos")!!, call.argument<String>("message")!!, call.argument<String>("topic")!!)
+            "setAutoResubscribe" -> FlutterAwsAmplifyPubSub.setAutoResubscribe(result, call.argument<Boolean>("enabled")!!)
+            "setCleanSession" -> FlutterAwsAmplifyPubSub.setCleanSession(result, call.argument<Boolean>("cleanSession")!!)
+            "connect" -> FlutterAwsAmplifyPubSub.connect(result, subscriptionEvent)
+            "disconnect" -> FlutterAwsAmplifyPubSub.disconnect(result)
+            "subscribeToTopic" -> FlutterAwsAmplifyPubSub.subscribeToTopic(result, subscriptionEvent, call.argument<String>("subscriptionTopic")!!, call.argument<Int>("qos")!!)
+            "unsubscribeTopic" -> FlutterAwsAmplifyPubSub.unsubscribeTopic(result, call.argument<String>("topic")!!)
+            "attachPolicy" -> FlutterAwsAmplifyPubSub.attachPolicy(result, call.argument<String>("policyName")!!, call.argument<String>("awsRegion")!!)
+            "publishString" -> FlutterAwsAmplifyPubSub.publishString(result, call.argument<String>("payload")!!, call.argument<String>("topic")!!, call.argument<Int>("qos")!!)
         }
     }
 
